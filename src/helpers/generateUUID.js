50c2320c7015f3281acbb6d41389c4e284b9d9a9
Ramki <kvraamkeydev@gmail.com>
2020-04-23 05:42:12 +0530

    uuid helper enabled

---
+++ src/helpers/generateUUID.js
@@ -0,0 +1,8 @@
+/**
+ * [generateUUID description]
+ *
+ * @return  {[string]}  [return uuidv4()]
+ */
+import uuidv4 from "uuid/v4";
+
+export const generateUUID = () => uuidv4();
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,3 +2,4 @@ export * from "./accessEnv";
 export * from "./validationError";
 export * from "./successResponse";
 export * from "./to";
+export * from "./generateUUID";
